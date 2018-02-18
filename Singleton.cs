fe7979f83334d1d9e9d32eb827535066d48087c3
Bennett Lynch <bennett.lynch@gmail.com>
2018-02-18 01:56:46 -0800

    Fix tower popup to forecfully initialize singleton

--- Singleton.cs
+++ Singleton.cs
@@ -3,4 +3,13 @@
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
     public static T Instance => instance ?? (instance = FindObjectOfType<T>());
     private static T instance { get; set; }
+
+    /// <summary>
+    /// Unfortunately, FindObjectOfType only finds active objects.
+    /// This method can be called if we need to deactivate a gameObject
+    /// that hasn't had its singleton initialized yet.
+    /// </summary>
+    protected void InitializeSingleton() {
+        var forcefullyInitialize = Instance;
+    }
 }
